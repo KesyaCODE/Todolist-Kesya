f588c18f60af7b690f3481e61ccfc0e3af753816
aistaqiemsy <a.istakim@gmail.com>
2025-05-07 15:35:07 +0700

    membuat UI admin penugasan selesai

--- resources/views/admin/penugasanSelesai.blade.php
+++ resources/views/admin/penugasanSelesai.blade.php
@@ -4,20 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo Selesai</title>
+
+    <link rel="stylesheet" type="text/css" href="/assets/bootstrap.css">
+    <style>
+        /* Menambahkan margin atau padding pada body */
+        body {
+            padding-top: 15px; /* Menambahkan jarak atas */
+        }
+    </style>
 </head>
 <body>
-    <table>
-        <tr>
-            <td>No.</td>
-            <td>Nama ToDo</td>
-        </tr>
-        @foreach ( $penugasanSelesai as $ps )
-        <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $ps->tugas }}</td>
-            <td>
-        </tr>
-        @endforeach
-    </table>
+    <div class="container-md">
+        <a class="btn btn-outline-primary rounded text-center" style="width: 180px;" href="/todo/admin">
+            Beranda
+        </a>
+        <a class="btn btn-outline-primary rounded text-center" style="width: 180px;" href="/admin/todo/dataPenugasan">
+            Rekap Penugasan
+        </a>
+        <hr>
+        <table class="table table-bordered border-primary">
+            <tr>
+                <td>No.</td>
+                <td>Nama Tugas</td>
+                <td>Waktu Penugasan</td>
+                <td>Waktu Selesai</td>
+                <td>Pemberi</td>
+                <td>Pelaksana</td>
+            </tr>
+            @foreach ( $penugasanSelesai as $ps )
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $ps->tugas }}</td>
+                <td>{{ $ps->waktu_mulai }}</td>
+                <td>{{ $ps->waktu_selesai }}</td>
+                <td>{{ $ps->tugas_dari }}</td>
+                <td>{{ $ps->tugas_untuk }}</td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
